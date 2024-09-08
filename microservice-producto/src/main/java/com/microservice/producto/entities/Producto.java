16c3f64474138e97db3d48e06624a74cc056a544
Samuel Rojas <samm-rojas@hotmail.com>
2024-09-08 11:43:48 -0500

    feat(producto): se agrega clase Producto

---
+++ microservice-producto/src/main/java/com/microservice/producto/entities/Producto.java
@@ -0,0 +1,37 @@
+package com.microservice.producto.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Builder
+@Table (name = "productos")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "stock")
+    private Integer stock;
+
+    @Column(name = "precio")
+    private BigDecimal precio;
+
+    @Column(name = "pedido_Id")
+    private Long pedidoId;
+}
