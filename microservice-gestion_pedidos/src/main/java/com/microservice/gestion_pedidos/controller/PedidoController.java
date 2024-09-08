7bdddee35465adec84baeaaee1f0da770a334841
Samuel Rojas <samm-rojas@hotmail.com>
2024-09-08 16:45:48 -0500

    feat(gestion_pedidos): se agrega controlador

---
+++ microservice-gestion_pedidos/src/main/java/com/microservice/gestion_pedidos/controller/PedidoController.java
@@ -0,0 +1,33 @@
+package com.microservice.gestion_pedidos.controller;
+
+import com.microservice.gestion_pedidos.entities.Pedido;
+import com.microservice.gestion_pedidos.service.IPedidosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/pedido")
+public class PedidoController {
+
+    @Autowired
+    private IPedidosService pedidosService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public  void savePedido(@RequestBody Pedido pedido){
+        pedidosService.save(pedido);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllPedidos(){
+        return ResponseEntity.ok(pedidosService.finAll());
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        return  ResponseEntity.ok(pedidosService.findById(id));
+    }
+
+}
