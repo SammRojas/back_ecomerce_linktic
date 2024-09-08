a7af899e1a3f0d74a0e1cd26357bbd07f0e6e0b0
Samuel Rojas <samm-rojas@hotmail.com>
2024-09-08 15:48:18 -0500

    feat(gestion_pedidos): se agrega clase pedido

---
+++ microservice-gestion_pedidos/src/main/java/com/microservice/gestion_pedidos/entities/Pedido.java
@@ -0,0 +1,34 @@
+package com.microservice.gestion_pedidos.entities;
+
+import com.microservice.producto.entities.Producto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@Table(name = "pedidos")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pedido {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(name = "fecha")
+        private Date fecha;
+
+        @Column(name = "total")
+        private BigDecimal total;
+
+        @Column(name = "productos")
+        private List<Producto> productos;
+}
