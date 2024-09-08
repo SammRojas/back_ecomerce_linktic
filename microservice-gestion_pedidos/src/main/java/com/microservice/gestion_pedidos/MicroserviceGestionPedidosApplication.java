5140d247e2376e2f7177e516a83acb92782b9a40
Samuel Rojas <samm-rojas@hotmail.com>
2024-09-08 11:05:22 -0500

    feat(gestion_pedidos): se agrega microservio para gestion_pedidos

---
+++ microservice-gestion_pedidos/src/main/java/com/microservice/gestion_pedidos/MicroserviceGestionPedidosApplication.java
@@ -0,0 +1,15 @@
+package com.microservice.gestion_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MicroserviceGestionPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGestionPedidosApplication.class, args);
+	}
+
+}
