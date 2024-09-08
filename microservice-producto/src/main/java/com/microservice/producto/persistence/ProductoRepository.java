90c7b91d9ed37e5d125e941bd9cb60a93a86eee2
Samuel Rojas <samm-rojas@hotmail.com>
2024-09-08 14:08:29 -0500

    feat(producto): se agrega capa de persistencia

---
+++ microservice-producto/src/main/java/com/microservice/producto/persistence/ProductoRepository.java
@@ -0,0 +1,18 @@
+package com.microservice.producto.persistence;
+
+import com.microservice.producto.entities.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    List<Producto> findOllByPedidoId(Long pedidoId);
+
+    @Query("SELECT p FROM productos p = WHERE p.pedidoId = :pedidoId")
+    List<Producto> findOllProducto(Long pedidoId);
+
+}
